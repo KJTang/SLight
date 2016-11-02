0c9ba1245d3e42647600bdb628b023784d1e357d
KJTang <kaijietang@outlook.com>
2016-11-02 20:11:30 +0800

    feat: add simple puzzle test

---
+++ Assets/Script/Puzzles/BoxPuzzle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class BoxPuzzle : Puzzle {
+    public Transform player;
+    private Vector3 distance;
+
+    void Start() {
+        Assert.IsNotNull(player);
+    }
+    
+    public override void Update() {
+        base.Update();
+
+        if (GetTriggerDown()) {
+            distance = player.position - transform.position;
+            Debug.Log(distance);
+        }
+        if (GetTrigger()) {
+            transform.position = player.position - distance;
+        }
+    }
+}
---
+++ Assets/Script/Puzzles/PlayerActionPuzzle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActionPuzzle : Puzzle {
+
+    void Start() {
+        isTriggered = false;
+    }
+	
+    public override void Update() {
+        // will not use default trigger way
+        // base.Update();
+        isTriggered = GameKernel.inputManager.GetKey(InputKey.Action);
+    }
+}
