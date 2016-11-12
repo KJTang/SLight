1f28a478fdafdd269296dc8066e8e5bca494d328
KJTang <kaijietang@outlook.com>
2016-11-12 11:10:05 +0800

    feat: tried RopePuzzle in Level002

---
+++ Assets/Script/Levels/Level002.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Level002 : Level {
+    public Level002() {
+        name = "Level002";
+    }
+
+    public override void OnEnter() {
+        SceneManager.LoadSceneAsync("Scene/Level002");
+        GameKernel.inputManager.Enable = true;
+    }
+
+    public override void Update() {}
+    
+    public override void OnExit() {
+        GameKernel.inputManager.Enable = false;
+    }
+}
