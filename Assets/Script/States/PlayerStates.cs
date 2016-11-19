09deac2e23025a4367773ab43bbd5d1d4ae3974a
KJTang <kaijietang@outlook.com>
2016-11-19 20:52:07 +0800

    feat: create player states: idle, walk, jump

--- Assets/Script/States/PlayerStates.cs
+++ Assets/Script/States/PlayerStates.cs
@@ -13,12 +13,18 @@ public class PlayerIdleState : State {
 
     public override void OnEnter() {
         // Debug.Log("Idle OnEnter");
+
         animator = stateMachine.gameObject.GetComponent<Animator>();
         Assert.IsNotNull(animator);
-        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsIdling", true);
     }
 
     public override void Update() {
+        // Jump
+        if (GameKernel.inputManager.GetKey(InputKey.Jump)) {
+            stateMachine.ChangeState("Jump");
+            return;
+        }
         // Walk
         if (GameKernel.inputManager.GetKey(InputKey.Left) || GameKernel.inputManager.GetKey(InputKey.Right)) {
             stateMachine.ChangeState("Walk");
@@ -28,11 +34,14 @@ public class PlayerIdleState : State {
 
     public override void OnExit() {
         // Debug.Log("Idle OnExit");
+        animator.SetBool("IsIdling", false);
     }
 }
 
 public class PlayerWalkState : State {
     Animator animator;
+    Rigidbody2D body;
+    PlayerControl script;
 
     public PlayerWalkState() {
         name = "Walk";
@@ -40,28 +49,103 @@ public class PlayerWalkState : State {
 
     public override void OnEnter() {
         // Debug.Log("Walk OnEnter");
+
         animator = stateMachine.gameObject.GetComponent<Animator>();
         Assert.IsNotNull(animator);
+        body = stateMachine.gameObject.GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(body);
+        script = stateMachine.gameObject.GetComponent<PlayerControl>();
+        Assert.IsNotNull(script);
         animator.SetBool("IsWalking", true);
     }
 
     public override void Update() {
+        // Jump
+        if (GameKernel.inputManager.GetKey(InputKey.Jump)) {
+            stateMachine.ChangeState("Jump");
+            return;
+        }
+        Vector2 velocity = stateMachine.gameObject.GetComponent<Rigidbody2D>().velocity;
         // Idle
-        if (!GameKernel.inputManager.GetKey(InputKey.Left) && !GameKernel.inputManager.GetKey(InputKey.Right)) {
+        if (!GameKernel.inputManager.GetKey(InputKey.Left) && 
+            !GameKernel.inputManager.GetKey(InputKey.Right)) {
             stateMachine.ChangeState("Idle");
             return;
         }
         // still Walk
-        Vector2 velocity = stateMachine.gameObject.GetComponent<Rigidbody2D>().velocity;
         Vector3 scale = stateMachine.gameObject.transform.localScale;
         if (velocity.x >= 0.0f) {
             stateMachine.gameObject.transform.localScale = new Vector3(Math.Abs(scale.x), scale.y, scale.z);
         } else {
             stateMachine.gameObject.transform.localScale = new Vector3(-Math.Abs(scale.x), scale.y, scale.z);
         }
+        // Impulse
+        if (Math.Abs(body.velocity.x) <= script.maxSpeed.x) {
+            if (GameKernel.inputManager.GetKey(InputKey.Left)) {
+                body.AddForce(new Vector2(-script.moveImpulse, 0.0f), ForceMode2D.Impulse);
+            }
+            if (GameKernel.inputManager.GetKey(InputKey.Right)) {
+                body.AddForce(new Vector2(script.moveImpulse, 0.0f), ForceMode2D.Impulse);
+            }
+        }
     }
 
     public override void OnExit() {
         // Debug.Log("Walk OnExit");
+        animator.SetBool("IsWalking", false);
+    }
+}
+
+public class PlayerJumpState : State {
+    Animator animator;
+    Collider2D collider;
+    Rigidbody2D body;
+    PlayerControl script;
+
+    public PlayerJumpState() {
+        name = "Jump";
+    }
+
+    public override void OnEnter() {
+        // Debug.Log("Jump OnEnter");
+
+        animator = stateMachine.gameObject.GetComponent<Animator>();
+        Assert.IsNotNull(animator);
+        collider = stateMachine.gameObject.GetComponent<Collider2D>();
+        Assert.IsNotNull(collider);
+        body = stateMachine.gameObject.GetComponent<Rigidbody2D>();
+        Assert.IsNotNull(body);
+        script = stateMachine.gameObject.GetComponent<PlayerControl>();
+        Assert.IsNotNull(script);
+        animator.SetBool("IsJumping", true);
+        if (Math.Abs(body.velocity.y) <= script.maxSpeed.y) {
+            body.AddForce(new Vector2(0.0f, script.jumpImpulse), ForceMode2D.Impulse);
+        }
+    }
+
+    public override void Update() {
+        // Idle
+        if (collider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            stateMachine.ChangeState("Idle");
+            return;
+        }
+        // still Jump
+        Vector3 scale = stateMachine.gameObject.transform.localScale;
+        // Impulse
+        if (Math.Abs(body.velocity.x) <= script.maxSpeed.x) {
+            if (GameKernel.inputManager.GetKey(InputKey.Left)) {
+                body.AddForce(new Vector2(-script.moveImpulse, 0.0f), ForceMode2D.Impulse);
+                stateMachine.gameObject.transform.localScale = new Vector3(-Math.Abs(scale.x), scale.y, scale.z);
+            }
+            if (GameKernel.inputManager.GetKey(InputKey.Right)) {
+                body.AddForce(new Vector2(script.moveImpulse, 0.0f), ForceMode2D.Impulse);
+                stateMachine.gameObject.transform.localScale = new Vector3(Math.Abs(scale.x), scale.y, scale.z);
+            }
+        }
+    }
+
+    public override void OnExit() {
+        // Debug.Log("Jump OnExit");
+        animator.SetBool("IsJumping", false);
     }
 }
--- Assets/Script/UnityScript/PlayerControl.cs
+++ Assets/Script/UnityScript/PlayerControl.cs
@@ -4,20 +4,23 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
     private Rigidbody2D body;
+    private Collider2D collider;
     private StateMachine stateMachine;
 
-    public float maxSpeed = 3.0f;
+    public Vector2 maxSpeed = new Vector2(3.0f, 3.0f);
     public float moveImpulse = 0.2f;
     public float jumpImpulse = 5.0f;
 
 	void Start () {
         body = (Rigidbody2D)gameObject.GetComponent<Rigidbody2D>();
+        collider = (Collider2D)gameObject.GetComponent<Collider2D>();
 
         stateMachine = new StateMachine();
         stateMachine.gameObject = gameObject;
         stateMachine.Init();
         stateMachine.AddState(new PlayerIdleState());
         stateMachine.AddState(new PlayerWalkState());
+        stateMachine.AddState(new PlayerJumpState());
         stateMachine.ChangeState("Idle");
     }
 	
@@ -33,19 +36,19 @@ public class PlayerControl : MonoBehaviour {
     }
     
     void DoMove() {
-        if (Math.Abs(body.velocity.x) <= maxSpeed) {
-            if (GameKernel.inputManager.GetKey(InputKey.Left)) {
-                body.AddForce(new Vector2(-moveImpulse, 0.0f), ForceMode2D.Impulse);
-            }
-            if (GameKernel.inputManager.GetKey(InputKey.Right)) {
-                body.AddForce(new Vector2(moveImpulse, 0.0f), ForceMode2D.Impulse);
-            }
-        }
+        // if (Math.Abs(body.velocity.x) <= maxSpeed) {
+        //     if (GameKernel.inputManager.GetKey(InputKey.Left)) {
+        //         body.AddForce(new Vector2(-moveImpulse, 0.0f), ForceMode2D.Impulse);
+        //     }
+        //     if (GameKernel.inputManager.GetKey(InputKey.Right)) {
+        //         body.AddForce(new Vector2(moveImpulse, 0.0f), ForceMode2D.Impulse);
+        //     }
+        // }
     }
 
     void DoAction() {
-        if (GameKernel.inputManager.GetKeyDown(InputKey.Jump) && body.velocity.y == 0) {
-            body.AddForce(new Vector2(0.0f, jumpImpulse), ForceMode2D.Impulse);
-        }
+        // if (GameKernel.inputManager.GetKeyDown(InputKey.Jump) && body.velocity.y == 0) {
+        //     body.AddForce(new Vector2(0.0f, jumpImpulse), ForceMode2D.Impulse);
+        // }
     }
 }
