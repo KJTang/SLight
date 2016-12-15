6e508488e2ea95e403edd26f4fdebb52303b3a22
KJTang <kaijietang@outlook.com>
2016-12-15 23:09:26 +0800

    refact: optimized LevelA2 & LevelA3

---
+++ Assets/Script/Puzzles/TrapDestroyer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class TrapDestroyer : Puzzle {
+    public Puzzle trapPuzzle;
+    public Vector3 moveDist = new Vector3(0, 0, 0);
+    public float moveTime = 1.0f;
+    public bool shouldDestroy = false;
+
+    private bool acting = false;
+
+    void Start() {
+        isTriggered = false;
+        Assert.IsNotNull(trapPuzzle);
+    }
+    
+    public override void Update() {
+        base.Update();
+        if (GetTrigger() && !acting) {
+            acting = true;
+            OnTriggerDown();
+        }
+        // if (!GetTrigger() && acting) {
+        //     acting = false;
+        //     OnTriggerUp();
+        // }
+    }
+
+    void OnTriggerUp() {
+        //
+    }
+
+    void OnTriggerDown() {
+        GameKernel.actionManager.RunAction(new ActionSequence(gameObject, 
+            new ActionMoveBy(trapPuzzle.gameObject, moveDist, moveTime), 
+            new ActionCallFunc(gameObject, (object obj) => {
+                    Debug.Log("Heyheyhey");
+                    if (shouldDestroy) {
+                        Destroy((GameObject)obj);
+                    }
+                }, 
+                trapPuzzle.gameObject
+            )
+        ));
+    }
+}
