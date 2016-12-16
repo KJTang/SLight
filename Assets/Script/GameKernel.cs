ccd13e5c73b0f542430e8f57598e06fc8c666829
KJTang <kaijietang@outlook.com>
2016-12-16 16:09:49 +0800

    refact: optimize splash scene

--- Assets/Script/GameKernel.cs
+++ Assets/Script/GameKernel.cs
@@ -38,8 +38,7 @@ public class GameKernel : MonoBehaviour {
     }
 
     void Start() {
-         levelManager.ChangeLevel(levelManager.CreateCommonLevel("LevelB1"), true);
-        // levelManager.ChangeLevel(new Level002(), true);
+         levelManager.ChangeLevel(levelManager.CreateCommonLevel("SplashScene"), true);
         // Debugging
         inputManager.Enable = true;
     }
---
+++ Assets/Script/UnityScript/SplashObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashObject : MonoBehaviour {
+    public string nextLevel = "StartScene";
+    private string videoPath = "splash.mp4";
+
+    void Start() {
+        StartCoroutine(PlayVideoCoroutine(videoPath));
+    }
+
+    IEnumerator PlayVideoCoroutine(string videoPath) {
+        Handheld.PlayFullScreenMovie(videoPath, Color.black, FullScreenMovieControlMode.CancelOnInput);    
+        yield return new WaitForEndOfFrame();
+        // Debug.Log("Video playback completed.");
+        GameKernel.actionManager.RunAction(new ActionSequence(gameObject, 
+            new ActionDelay(gameObject, 0.5f), 
+            new ActionCallFunc(gameObject, (object obj) => {
+                    GameKernel.levelManager.ChangeLevel(
+                        GameKernel.levelManager.CreateCommonLevel(nextLevel), 
+                        true
+                    );
+                }, 
+                null
+            )
+        ));
+    }
+}
