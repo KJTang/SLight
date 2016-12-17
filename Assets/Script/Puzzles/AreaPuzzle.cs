b905eb214c3f1c6221ee77a323d14f9692fc608e
THTBa <412199815@qq.com>
2016-12-17 20:56:00 +0800

    feat: creat a new levelC1.Change the Jump Animation.

---
+++ Assets/Script/Puzzles/AreaPuzzle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaPuzzle : Puzzle {
+    // Update is called once per frame
+    Collider2D c;
+    void Start()
+    {
+        c = gameObject.GetComponent<PolygonCollider2D>();
+        if (c)
+        {
+            c.isTrigger = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")//另一物体是player
+        {
+            //set state
+            isTriggered = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")//另一物体是player
+        {
+            //set state
+            isTriggered = false;
+        }
+    }
+}
---
+++ Assets/Script/Puzzles/TutorialPuzzle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor.Animations;
+using System.Collections;
+using System.Collections.Generic;
+public class TutorialPuzzle : MonoBehaviour
+ {
+    public List<Puzzle> puzzles = new List<Puzzle>();
+    // Use this for initialization
+    private bool[] l = { false,false,false,false };
+    void Start()
+    {
+        int i = 0;
+        foreach (Puzzle p in puzzles)
+        {
+            Debug.Log(transform.GetChild(i) + "initset");
+            transform.GetChild(i).GetComponent<Animator>().enabled = false;
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        foreach (Puzzle p in puzzles)
+        {
+            if (i == 1 && p.GetTriggerDown()) Debug.Log(p);
+            if (p.GetTriggerDown() && !l[i])
+            {
+                Debug.Log(transform.GetChild(i) + "settrue");
+                transform.GetChild(i).GetComponent<Animator>().enabled = true;
+                l[i] = true;
+            }
+            if (p.GetTriggerUp())
+            {
+                Debug.Log(transform.GetChild(i) + "setfalse");
+                transform.GetChild(i).GetComponent<Animator>().enabled = false;
+                transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = null;
+            }
+            i++;
+        }
+    }
+}
