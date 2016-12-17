a57474d30b3fcd6c9e0533c2a6f267e6ed45c3d1
THTBa <412199815@qq.com>
2016-12-18 02:42:42 +0800

    refact: Debug.

--- Assets/Script/Puzzles/CameraControl/Sp_B1.cs
+++ Assets/Script/Puzzles/CameraControl/Sp_B1.cs
@@ -2,28 +2,31 @@
 using System.Collections;
 
 public class Sp_B1 : Sp_B2 {
-    public Puzzle sp1, sp2, sp3;
+    public Puzzle sp1, sp2, sp3, sp4;
     public Vector3 sd1, sd2, sd3;
     public Transform ball;
+
+    private bool p4_down;
     // Use this for initialization	
     // Update is called once per frame
 
     void Start()
     {
+        p4_down = true;
     }
 	public override void Update () {
         sd2 = ball.position - transform.position;
         sd2.z = 0;
+        Debug.Log(flag);
         if (sp1.GetTriggerDown() && flag)
         {
-            //Debug.Log("33");
+            Debug.Log("33");
             flag = false;
-            GameKernel.inputManager.Enable = false;
             GameKernel.actionManager.RunAction(new ActionSequence(gameObject, 
                 new ActionMoveBy(gameObject,
                     -sd1,
                     1.0f),
-                new ActionDelay(gameObject, 1.0f),
+                new ActionDelay(gameObject, 2.0f),
                 new ActionMoveBy(gameObject,
                     sd1,
                     1.0f),
@@ -82,6 +85,14 @@ public class Sp_B1 : Sp_B2 {
                     GameKernel.inputManager.Enable = true;
                 }, gameObject)
             ));
+        }
+        if (sp4.GetTriggerDown() && flag &&p4_down)
+        {
+            Debug.Log("33");
+            flag = false;
+            GameKernel.inputManager.Enable = false;
+            p4_down = false;
+            flag = true;
         }
         base.Update();
 
