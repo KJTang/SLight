da52fb236fc2cccf3594abf9ad72af250590105b
KJTang <kaijietang@outlook.com>
2016-12-17 21:46:43 +0800

    feat: LevelB1

---
+++ Assets/Script/Puzzles/TimeDelayPuzzle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class TimeDelayPuzzle : Puzzle {
+    public Puzzle timeStart;
+    public float delayTime = 0.0f;
+
+    private bool isStart = false;
+    private float timer = 0.0f;
+
+    void Start () {
+        isTriggered = false;
+        Assert.IsNotNull(timeStart);
+    }
+
+    public override void Update () {
+        // base.Update();
+        if (isTriggered) {
+            return;
+        }
+        if (!isStart && timeStart.isTriggered) {
+            isStart = true;
+            timer = 0.0f;
+        }
+        if (isStart) {
+            timer += Time.deltaTime;
+        }
+        if (timer >= delayTime) {
+            isTriggered = true;
+        }
+    }
+}
